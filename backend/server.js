0956c3ae2672a622a23a282fc0d9967da53c0639
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-11-07 17:26:51 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors()); 
+app.use(express.json()); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
