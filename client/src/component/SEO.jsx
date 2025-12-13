8c8369f5315f888b59945b80e5b132f446f2166f
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-12-13 21:34:54 +0530

    api fix

--- client/src/component/SEO.jsx
+++ client/src/component/SEO.jsx
@@ -47,3 +47,5 @@ const SEO = ({
 };
 
 export default SEO;
+
+
--- client/src/utils/apiConnector.js
+++ client/src/utils/apiConnector.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
-
+const API = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: `${API}/api`,
 });
 
 
