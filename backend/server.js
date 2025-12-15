7d06fe977e180b6e39826097fa4b8853279ea231
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-12-15 19:04:21 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Parse JSON bodies
 app.use(express.json());
@@ -38,14 +38,6 @@ app.use('/api/products', productsRouter);
 app.use('/api/contact', contactRouter);
 app.use('/api/auth', authRouter);
 
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
-
-
-app.get("/api/health", (req, res) => {
-  res.status(200).json({ status: "OK" });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
